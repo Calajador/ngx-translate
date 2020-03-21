cf82ee8ba743bb32ae8b3e935b033d2e1d5c1c24
Calajador <calajador@hotmail.com>
2020-03-21 08:09:51 +0100

    Primer Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TranslationComponent } from './translation/translation.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TranslationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
